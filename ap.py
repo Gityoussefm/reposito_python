d5f46ccad0ab00bb67d8fdb567138b9407c16eab
youssef <moryjoe007@gmail.com>
2024-05-17 12:32:48 +0200

    first commit

---
+++ ap.py
@@ -0,0 +1,12 @@
+print("hola")
+a = 5                               
+b = 10                                                                
+a, b = b, a                                                                 
+print(a) # 10                               
+print(b)
+# -*- coding: utf-8 -*-
+# DEFINIR
+def saludar():
+    print("Hola Andrea")
+#EJECUTAR
+saludar()    
