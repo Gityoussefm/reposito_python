67d9fc4185dde3a4a5b2dcce59d9780a86ed7414
youssef <moryjoe007@gmail.com>
2024-05-20 22:53:30 +0200

    push

--- ap.py
+++ ap.py
@@ -1,13 +1,73 @@
-print("hola")
-a = 5                               
-b = 10                                                                
-a, b = b, a                                                                 
-print(a) # 10                               
-print(b)
+
+
 # -*- coding: utf-8 -*-
-# DEFINIR
+# PODEMOS VER EL SCOPE DE LAS VARIABLES Y FUNCIONES
+name = "Vicents"
+
 def saludar():
-    print("Hola Andrea")
-#EJECUTAR
+    print("Hola " +  name)
+
+print(name)
 saludar()    
-print("hi")
+
+
+
+name = "Vicents"
+
+def saludar():
+    name = "Sergi"
+    print("Hola " +  name)
+
+saludar()
+print(name)
+
+
+
+import datetime
+actual_year = datetime.date.today().year
+
+user1 = {"name": "Krisel", "isAdmin": True, "bith": 1989, "parent": False}
+user2 = {"name": "JM", "isAdmin": True, "bith": 2014, "parent": True}
+
+def can_access(user):
+    age = actual_year - user["bith"]
+    if not user["isAdmin"] or age < 18:
+        return False
+    return True
+
+def access_sys(user): 
+    if not can_access(user):
+        print(user["name"] + " Acceso denegado") 
+        return
+
+        print(user["name"] + " Ha accedido al sistema ")
+        print("Ha accedido al sistema ")
+        print("Ha accedido al sistema ")
+        print("Ha accedido al sistema ")
+        print("Ha accedido al sistema ")
+
+access_sys(user1)    
+access_sys(user2)
+
+# -*- coding: utf-8 -*-
+
+
+# -*- Asigna tu nombre a una variable -*-
+miNombre = "Andrea"
+
+print("Mi nombre es: " + miNombre)
+
+
+
+# -*- Imprime el número de caracteres de tu nombre -*-
+miNombre = "Andrea"
+nombre_caracteres = len(miNombre)
+
+print("Mi nombre tiene  " + str(nombre_caracteres))
+
+
+# -*- Imprime tu nombre en mayúsculas -*-
+miNombre = "Andrea"
+nombre_mayusculas = miNombre.upper()
+
+print(nombre_mayusculas)
--- d.py
+++ d.py
@@ -1,1 +1,40 @@
 print("hello world")
+
+# -*- coding: utf-8 -*-
+# DEFINIR
+def saludar():
+    print("Hola Andrea")
+#EJECUTAR
+saludar()    
+
+
+
+
+# DEFINIR 
+def saludar(name):
+    print("Hola " + name)
+#EJECUTAR
+saludar("Sergi") 
+saludar("Andre") 
+saludar("JM") 
+saludar("")
+
+
+# DEFINIR 
+def saludar(name):
+    return "Hola " + name
+#EJECUTAR
+saludo = "voy a visitar: " + saludar("María")
+print(saludo)
+
+#ejemplo
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[1])
+
+myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+#print(myMixedTypeList)
+
+
+
